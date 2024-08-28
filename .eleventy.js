54e354be86021802192925c4a276381ac3f5732a
hamzabawumia <hamzabawumia@yahoo.com>
2024-08-28 09:18:29 -0700

    getting better

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
   // Define input and output directories
-  eleventyConfig.addPassthroughCopy("learn_french_in_45_days/_includes"); // Optional: Copy static assets, if any
+  eleventyConfig.addPassthroughCopy("mew_score/_includes"); // Optional: Copy static assets, if any
   
   // Add global data
   eleventyConfig.addGlobalData('baseUrl', '/');
@@ -11,8 +11,8 @@ module.exports = function(eleventyConfig) {
   // Return directory settings
   return {
     dir: {
-      input: "learn_french_in_45_days", // Source directory
-      output: "learn_french_in_45_days/_site" // Build output directory
+      input: "mew_score", // Source directory
+      output: "mew_score/_site" // Build output directory
     }
   };
 };
