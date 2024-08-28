169f2241ddb5b5f6677b1de6f5b76afbf9aa05d7
hamzabawumia <hamzabawumia@yahoo.com>
2024-08-28 13:40:33 -0700

    Thank God

--- .eleventy.js
+++ .eleventy.js
@@ -2,13 +2,14 @@ module.exports = function(eleventyConfig) {
   // Define input and output directories
   eleventyConfig.addPassthroughCopy("mew_score/_includes"); // Optional: Copy static assets, if any
   
-  // Add other configurations if needed
+  eleventyConfig.setTemplateFormats(["njk", "html", "md"]);
 
   // Return directory settings
   return {
     dir: {
       input: "mew_score", // Source directory
-      output: "mew_score/_site" // Build output directory
+      output: "mew_score/_site", // Build output directory
+      includes: "mew_score/_includes" // custom includes directory
     }
   };
 };
