aceabbb82aaf2729f088b489c2a3f9c31b7e6aa8
hamzabawumia <hamzabawumia@yahoo.com>
2024-08-28 10:48:58 -0700

    fixed the issue

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,6 @@ module.exports = function(eleventyConfig) {
   // Define input and output directories
   eleventyConfig.addPassthroughCopy("mew_score/_includes"); // Optional: Copy static assets, if any
   
-  // Add global data
-  eleventyConfig.addGlobalData('baseUrl', '/');
-  // now i can specigy the baseUrl in my code as {{ baseUrl }}
-  
   // Add other configurations if needed
 
   // Return directory settings
